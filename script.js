65d67c5ce6f248d0c58406679de695f2789e76df
christerross <47217893+christerross@users.noreply.github.com>
2019-04-28 17:13:26 +0200

    Question 2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function whyMessage() {
-    alert("Why would you do that?")
+fetch('https://jsonplaceholder.typicode.com/todos/format=json')
+.then(result => result.json())
+.then((res) => {
+    createList(res);
+})
+.catch(err => console.log(err))
+
+
+function createList(result) {
+    var details = document.getElementById("todoList");
+    details.innerHTML += "<span>UserId: " + result.userId + "</span>";
+    details.innerHTML += "<span>Id: " + result.id + "</span>";
+    details.innerHTML += "<span>Title: " + result.title + "</span>";
+    details.innerHTML += "<span>Completed: " + result.completed + "</span>";
 }
